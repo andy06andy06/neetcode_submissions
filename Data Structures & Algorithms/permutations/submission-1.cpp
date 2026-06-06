eaa5a7178c9e702b699e37f5b5030b9571ac4c71
Andy <109472382+andy06andy06@users.noreply.github.com>
2026-06-06 12:17:38 +0800

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void dfs(vector<int> &nums, unordered_set<int> &hashset, vector<vector<int>> &res, vector<int> &temp){
+        if(temp.size()==nums.size()){
+            res.push_back(temp);
+            return;
+        }
+        for(int j = 0; j<nums.size(); j++){
+            if(!hashset.count(nums[j])){
+                hashset.insert(nums[j]);
+                temp.push_back(nums[j]);
+                dfs(nums, hashset, res, temp);
+                hashset.erase(nums[j]);
+                temp.pop_back();
+            }
+        }
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        unordered_set<int> hashset;
+        vector<vector<int>> res;
+        vector<int> temp;
+        dfs(nums, hashset, res, temp);
+        return res;
+    }
+};
